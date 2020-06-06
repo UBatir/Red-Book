69e99d1a013a7d5822e111770222215a351bc381
UBatir <urazbaaev.br@gmail.com>
2020-06-06 09:41:04 +0500

    fragments changed

--- app/src/main/java/com/example/redbook/ui/animal/AnimalFragment.kt
+++ app/src/main/java/com/example/redbook/ui/animal/AnimalFragment.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.redbook.R
 import com.example.redbook.data.RedBookDatabase
 import com.example.redbook.data.dao.AnimalDao
+import com.example.redbook.ui.MainActivity
 import kotlinx.android.synthetic.main.fragmet_animal.*
 
 class AnimalFragment:Fragment(R.layout.fragmet_animal) {
@@ -18,11 +19,12 @@ class AnimalFragment:Fragment(R.layout.fragmet_animal) {
         super.onViewCreated(view, savedInstanceState)
         recyclerView.adapter=adapter
         recyclerView.addItemDecoration(DividerItemDecoration(requireContext(),DividerItemDecoration.VERTICAL))
+        val type=arguments?.getInt(MainActivity.TYPE_ID)?:1
         dao=RedBookDatabase.getInstance(requireContext()).dao()
-        setData()
+        setData(type)
     }
 
-    private fun setData(){
-        adapter.models=dao.getAllAnimals()
+    private fun setData(type:Int){
+        adapter.models=dao.getAllAnimals(type)
     }
 }

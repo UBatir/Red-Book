7bf3506656aff024fb51abe685b2f2f0d75ba1c5
UBatir <urazbaaev.br@gmail.com>
2020-06-18 22:54:26 +0500

    Added search

--- app/src/main/java/com/example/redbook/ui/animal/AnimalFragment.kt
+++ app/src/main/java/com/example/redbook/ui/animal/AnimalFragment.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.redbook.R
 import com.example.redbook.data.RedBookDatabase
 import com.example.redbook.data.dao.AnimalDao
+import com.example.redbook.data.model.Animal
 import com.example.redbook.ui.MainActivity
 import com.example.redbook.ui.detail.DetailActivity
 import kotlinx.android.synthetic.main.fragmet_animal.*
@@ -25,6 +27,10 @@ class AnimalFragment:Fragment(R.layout.fragmet_animal),AnimalItemClickListener {
         val type=arguments?.getInt(MainActivity.TYPE_ID)?:1
         dao=RedBookDatabase.getInstance(requireContext()).dao()
         setData(type)
+        etSearch.addTextChangedListener {
+            val result:List<Animal> =dao.searchAnimal(type,"${it.toString()}%")
+            adapter.models=result
+        }
     }
 
     private fun setData(type:Int){

4554caf327eaf78a7daa314e3bfe0dcdbda3a3dd
UBatir <urazbaaev.br@gmail.com>
2020-07-02 10:57:56 +0500

    added mvp

--- app/src/main/java/com/example/redbook/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/redbook/ui/detail/DetailActivity.kt
@@ -11,15 +11,15 @@ import com.example.redbook.data.dao.AnimalDao
 import com.example.redbook.data.model.Animal
 import kotlinx.android.synthetic.main.activity_detail.*
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity(),DetailView {
     companion object{
         const val ANIMAL_ID="animalId"
     }
 
     private var animalId:Int=0
     lateinit var dao:AnimalDao
-    lateinit var currentAnimal:Animal
     private var menuItem:MenuItem?=null
+    private lateinit var presenter: DetailPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,48 +30,42 @@ class DetailActivity : AppCompatActivity() {
 
         animalId=intent.getIntExtra(ANIMAL_ID,0)
         dao=RedBookDatabase.getInstance(this).dao()
-        currentAnimal=dao.getAnimalById(animalId)
-        tvStatusContent.text=currentAnimal.status
-        tvHabitatContent.text=currentAnimal.habitat
-        tvPropagationContent.text=currentAnimal.propagation
-        tvQuantityContent.text=currentAnimal.quantity
-        tvLifestyleContent.text=currentAnimal.lifestyle
-        tvLimitingFactorsContent.text=currentAnimal.limitingFactors
-        tvBreedingContent.text=currentAnimal.breeding
-        tvSecurityContent.text=currentAnimal.security
-        Glide
-            .with(this)
-            .load(resources.getIdentifier("picture$animalId","drawable",packageName))
-            .into(ivDetail)
+        presenter= DetailPresenter(dao,this)
+        presenter.getAnimalById(animalId)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_detail,menu)
         menuItem=menu?.findItem(R.id.item_bookmark)
-        setFavouriteIcon()
+        presenter.setFavouriteStatus()
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             android.R.id.home->finish()
-            R.id.item_bookmark->setFavourite()
+            R.id.item_bookmark->presenter.setFavourite()
         }
         return super.onOptionsItemSelected(item)
     }
-    private fun setFavourite(){
-        if (currentAnimal.isFavourite==null) currentAnimal.isFavourite=1
-        else currentAnimal.isFavourite=1-currentAnimal.isFavourite!!
-        setFavouriteIcon()
-        dao.updateAnimal(currentAnimal)
+
+
+    override fun setData(animal: Animal) {
+        Glide
+            .with(this)
+            .load(resources.getIdentifier("picture${animal.id}","drawable",packageName))
+            .into(ivDetail)
+        tvStatusContent.text=animal.status
+        tvHabitatContent.text=animal.habitat
+        tvPropagationContent.text=animal.propagation
+        tvQuantityContent.text=animal.quantity
+        tvLifestyleContent.text=animal.lifestyle
+        tvLimitingFactorsContent.text=animal.limitingFactors
+        tvBreedingContent.text=animal.breeding
+        tvSecurityContent.text=animal.security
     }
 
-    private fun setFavouriteIcon(){
-        if(currentAnimal.isFavourite==1){
-            menuItem?.setIcon(R.drawable.ic_baseline_bookmark_24)
-        }
-        else{
-            menuItem?.setIcon(R.drawable.ic_baseline_bookmark_border_24)
-        }
+    override fun setFavouriteIcon(id: Int) {
+        menuItem?.setIcon(id)
     }
 }
